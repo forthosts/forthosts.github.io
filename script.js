5ae8e073d63ca797c0320ae400db1a80a943c342
kotuk0iq <hdyhhdh75762@gmail.com>
2025-09-28 20:20:30 +0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.forEach(l => l.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+});
